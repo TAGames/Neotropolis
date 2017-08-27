5951562b964a390159dc292439c46ecd330c7d6f
Thomas Pommerening <pommermann@hotmail.de>
2017-08-27 18:49:49 +0200

    Waitergame in progress 2D atm changing to 3D sideview in next commit

---
+++ Assets/Scripts/GuestBehaviour.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuestBehaviour : MonoBehaviour {
+
+    GameObject waiterGame;
+    Transform chair;
+    public string food = "I Am Error";
+    int points = 10;
+    float speed = 0.05f;
+    private float endOfWaiting = 5;
+    private float timer = 0;
+    public bool alive = true;
+    public State state;
+
+    public enum State
+    {
+        Waiting,
+        Order,
+        WaitFood,
+        Exit
+    }
+
+    // Use this for initialization
+    void Start () {
+        waiterGame = GameObject.Find("WaiterGame");
+        StartCoroutine("FSM");
+	}
+    void SetFood(string food) //Set At spawn by WaiterGame
+    {
+        this.food = food;
+    }
+    void SetChair(Transform destination)
+    {
+        chair = destination;
+    }
+
+
+    IEnumerator FSM() //Finite State Machine
+    {
+        while (alive)
+        {
+            switch (state)
+            {
+                case State.Waiting:
+                    Waiting();
+                    break;
+                case State.Order:
+                    Order();
+                    break;
+                case State.WaitFood:
+                    WaitFood();
+                    break;
+                case State.Exit:
+                    Exit();
+                    break;
+            }
+            yield return null;
+        }
+    }
+    //StateMethods
+    void Waiting()
+    {
+        timer += Time.deltaTime;
+        /*
+        if(this.transform != chair)
+        {
+            if(transform.position.x < chair.position.x)
+            {
+                transform.position += new Vector3(speed, 0, 0);
+            }
+            else if(transform.position.x > chair.position.x)
+            {
+                transform.position -= new Vector3(speed, 0, 0);
+            }
+            else if (transform.position.y < chair.position.y)
+            {
+                transform.position += new Vector3(0, speed, 0);
+            }
+            else if (transform.position.y > chair.position.y)
+            {
+                transform.position -= new Vector3(0, speed, 0);
+            }
+         
+        }
+        */
+        //Debug.Log();
+        if(timer >= endOfWaiting)
+        {
+            timer = 0;
+            state = State.Order;
+        }
+    }
+    void Order()
+    {
+        timer += Time.deltaTime;
+        if(timer >= 10)
+        {
+            Debug.Log("Angry Guest");
+        }
+    }
+    void WaitFood()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= 20)
+        {
+            Debug.Log("Angry Guest");
+        }
+    }
+    void Exit()
+    {
+
+    }
+}
---
+++ Assets/Scripts/WaiterGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaiterGame : MonoBehaviour
+{
+    public GameObject GuestPrefab;
+    public Transform Spawnpoint;
+    List<string> guestList = new List<string>();
+    string[] FoodArray = { "Kartoffel Ramus", "Käse"};
+    public Transform[] chairArray;
+    // Use this for initialization
+    void Start()
+    {
+        Physics.gravity = new Vector3(0, 0, -0.1f);
+        spawnGuest();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void spawnGuest()
+    {
+       GameObject Guest = Instantiate(GuestPrefab, Spawnpoint);
+        Guest.SendMessage("SetFood", FoodArray[(int)Random.Range(0, FoodArray.Length)]);
+        Guest.SendMessage("SetChair", chairArray[0]);
+    }
+
+}
