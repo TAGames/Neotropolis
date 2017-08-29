a05e605298070d90db2f97a400301c6392d54f58
Zude <Alex.93.loeffler@me.com>
2017-08-29 11:57:12 +0200

    SlotMaschine
    Bald Fertig

---
+++ Assets/Scripts/SideScroll/SlotGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class SlotGame : MonoBehaviour {
+
+	public GameObject[] myPlayStones; 
+	public Sprite[] slotSprites;
+
+	public void PlayGame (){
+
+		int sameCount=0;
+		int bestCount=0;
+		foreach (GameObject go in myPlayStones) {
+			go.GetComponent<Image> ().sprite = slotSprites [Random.Range (0, slotSprites.Length)];
+
+		}
+
+		foreach (Sprite spri in slotSprites) {
+			foreach (GameObject go in myPlayStones) {
+				if (spri == go.GetComponent<Image> ().sprite) {
+					sameCount++; 
+				} 
+			}
+			if (sameCount > bestCount) {
+				bestCount = sameCount;
+
+			}
+			sameCount = 0; 
+		}
+
+		Debug.Log (bestCount);
+		sameCount = 0;
+		bestCount = 0; 
+	}
+
+}
---
+++ Assets/Scripts/SideScroll/SlotMaschine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotMaschine : MonoBehaviour {
+
+	public GameObject myObject; 
+	bool myBool = false; 
+	// Use this for initialization
+	void Start () {
+		myObject.SetActive (myBool);
+
+	}
+	
+
+	void OnTriggerStay2D(Collider2D player){
+
+		if(Input.GetKeyDown(KeyCode.F)){
+			myBool = !myBool; 
+			myObject.SetActive (myBool);}
+			
+
+	}
+
+	void OnTriggerExit2D(Collider2D player){
+		myBool = false;
+		myObject.SetActive (myBool);
+
+	}
+
+
+
+}
