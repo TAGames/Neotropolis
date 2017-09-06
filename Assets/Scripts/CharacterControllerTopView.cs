5aecf1bf080897f4ecee965707f72cc5c9e89798
Thomas Pommerening <pommermann@hotmail.de>
2017-09-06 22:48:26 +0200

    Added some Sprites. First Step for 2D TopView

---
+++ Assets/Scripts/CharacterControllerTopView.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerTopView : MonoBehaviour {
+
+    public bool canWalk = true;
+    public float speed = 1;
+    Rigidbody2D rb2D;
+	// Use this for initialization
+	void Start () {
+        rb2D = transform.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Move();
+	}
+    void Move()
+    {
+        if (canWalk)
+        {
+            if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+            {
+                //rb2D.transform.position = new Vector2(rb2D.transform.position.x, rb2D.transform.position.y * Input.GetAxisRaw("Vertical") * speed);
+            }
+        }
+    }
+}
