3c897bd0dc3d174fafb6f2e53b1edf822ff267e8
Zude <Alex.93.loeffler@me.com>
2017-09-09 03:47:28 +0200

    tooltip script fast fertig

---
+++ Assets/Scripts/SideScroll/ToolTip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolTip : MonoBehaviour {
+	public GameObject myPrefab;
+	private GameObject myObject;
+	public string myString; 
+
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("test");
+		myObject=Instantiate(myPrefab); 
+		myObject.transform.parent=transform;
+
+		myObject.GetComponentInChildren<Text> ().text = myString;
+
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		Destroy (myObject);
+	}
+}
