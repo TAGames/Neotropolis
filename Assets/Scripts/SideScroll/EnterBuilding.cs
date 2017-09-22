7caff0ec56cadd3912b594c58af0696b0fe5fd15
Zude <Alex.93.loeffler@me.com>
2017-09-22 23:48:17 +0200

    CardShop
    Cardshop kann jetzt betreten werden

---
+++ Assets/Scripts/SideScroll/EnterBuilding.cs
@@ -0,0 +1,47 @@
+﻿// Script, zum entern von Gebäuden (Wand verschwindet mit collidern, neue Kollider die drinne sind, tauchen auf 
+// Script am Hauptkollider anbringen (also Parent) 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterBuilding : MonoBehaviour {
+
+
+	[Tooltip("Das GameObject, was verschwinden soll, wenn man interagiert (Die Wand)")]
+	public GameObject goDeactivate;
+	[Tooltip("Das GameObject, was aktiviert werden soll (leeres GO mit neuen Collidern und neuer Logik")]
+	public GameObject goActivate; 
+	private bool switcher = true; 
+	private bool inTrigger = false; 
+	// Use this for initialization
+	void Start () {
+		goDeactivate.SetActive (switcher); 
+		goActivate.SetActive (!switcher); 
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F) && (inTrigger == true)) {
+			switcher = !switcher; 
+
+			goDeactivate.SetActive (switcher); 
+			goActivate.SetActive (!switcher); 
+
+		}
+	}
+
+
+
+	void OnTriggerEnter2D(Collider2D other){
+		inTrigger = true;
+
+}
+
+	void OnTriggerExit2D(Collider2D other){
+		inTrigger = false; 
+	}
+
+}
---
+++ Assets/Scripts/SideScroll/Fahrstuhl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fahrstuhl : MonoBehaviour {
+
+	private GameObject player; 
+
+	[Tooltip("Speed des Fahrstuhls")]
+	public float elevatorSpeed = 1f; 
+	private bool inTrigger = false; 
+	private bool eleStart = false; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F) && (inTrigger == true)) {
+			eleStart = true; 
+
+			player.GetComponent<BoxCollider2D> ().enabled = false; 
+
+			player.GetComponent<PlayerPlatformerController> ().enabled = false; 
+
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		player =  other.gameObject; 
+		inTrigger = true; 
+	}
+
+	void FixedUpdate(){
+
+		if (eleStart){
+			
+			transform.position = new Vector3 (transform.position.x, transform.position.y - 0.1f ,transform.position.z );
+
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		inTrigger = false; 
+	}
+}
